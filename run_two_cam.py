146b4d71856da5818d62b0ca301af70c2a24fa5b
sam <sam@silkron.com>
2022-02-15 16:37:48 +0800

    add file for subprocess

--- run_two_cam.py
+++ run_two_cam.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.Popen(f'python queue_read_frame.py --src 0')
-subprocess.Popen(f'python queue_read_frame.py --src 1')
+subprocess.Popen(f'python queue_read_frame.py --src 0 --save --save_dir "./frames/src_0"')
+subprocess.Popen(f'python queue_read_frame.py --src 1 --save --save_dir "./frames/src_1"')
