3b792ed5295b6f9bc4ead51365e1f3716b196389
sam <sam@silkron.com>
2022-02-16 10:52:15 +0800

    function write frame's data timestamp

---
+++ write_frame_timestamp.py
@@ -0,0 +1,11 @@
+from datetime import datetime 
+
+def get_date_timestamp(save_dir, count):      
+    timestamp_csv = f'{save_dir}/timestamp.csv'
+    
+    if count == 1:
+        with open(timestamp_csv, 'w') as f:
+            f.write('Frame, Date_Timestamp')
+            
+    with open(timestamp_csv, 'a') as f:
+        f.write(f'\n{count}, {datetime.now()}')
